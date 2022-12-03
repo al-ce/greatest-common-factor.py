98ac0866fb785084ba52bb2139d8b80a44b6cd90
Alessandro Ceserani <aceserani@gmail.com>
2022-12-03 08:20:43 -0600

    handle no common factors, clean up main()
    handle no common factors, clean up main()

--- gcf.py
+++ gcf.py
@@ -22,6 +22,13 @@ class PrimeFactorTree:
         self.primes = []
         self.root = self.prime_factorization(value)
 
+    def is_prime(self, n):
+        """Return True if n is prime, else return False."""
+        for i in range(2, int(n/2)):
+            if (n % i) == 0:
+                return False
+        return True
+
     def prime_factorization(self, n: int):
         """Prime Factor Tree generator."""
 
@@ -42,13 +49,6 @@ class PrimeFactorTree:
 
         return node
 
-    def is_prime(self, n):
-        """Return True if n is prime, else return False."""
-        for i in range(2, int(n/2)):
-            if (n % i) == 0:
-                return False
-        return True
-
 
 class GCFCalculator:
     def __init__(self, nums: list):
@@ -58,32 +58,6 @@ class GCFCalculator:
             "Prime Factorization": self.gcf_by_prime_factorization,
         }
         self.results = self.get_gcf_and_algo_runtimes(nums)
-
-    def convert_nums_to_str(self, nums):
-        """Convert a list of numbers to a csv row."""
-        nums_str = "Numbers:"
-        for num in nums:
-            nums_str += f" {num}"
-        return nums_str
-
-    def get_gcf_and_algo_runtimes(self, nums):
-        """Return the GCF of the list of nums and the runtime of each
-        implemented algorithm as a string."""
-
-        gcf_values = {}
-        runtimes = ""
-
-        for name, algorithm in self.algos.items():
-            gcf, runtime = self.get_algo_data(algorithm, nums)
-            gcf_values[name] = gcf
-            runtimes += f"{name} Algorithm runtime: {runtime}\n"
-
-        self.bug_check(gcf_values)
-
-        num_str = self.convert_nums_to_str(nums)
-        gcf = list(gcf_values.values())[0]
-        results = f"Numbers: {num_str}\nGCF: {gcf}\n{runtimes}"
-        return results
 
     def bug_check(self, gcf_values: dict):
         """Raise an error if the algorithms return differing values."""
@@ -99,15 +73,22 @@ class GCFCalculator:
             msg += f"{algo_name:<20} algorithm returned {gcf}\n"
         raise Exception(msg)
 
-    def get_algo_data(self, algorithm, nums: list):
-        """Return gcf (int) and runtime (str) for a given GCF algorithm."""
+    def convert_nums_to_str(self, nums):
+        """Convert a list of numbers to a csv row."""
+        nums_str = "Numbers:"
+        for num in nums:
+            nums_str += f" {num}"
+        return nums_str
 
-        start = datetime.now()
-        gcf = algorithm(nums)
-        end = datetime.now()
-
-        td = (end - start).total_seconds() * 10**3
-        return gcf, f"{td:.03f}ms"
+    def factorization(self, n: int) -> list:
+        """Get all the factors of an integer"""
+        s = sqrt(n).__floor__()  # call .__floor__ to round down
+        factors = []
+        for i in range(2, s+1):
+            q = n/i  # quotient
+            if q.is_integer():
+                factors += [int(q), i]
+        return factors
 
     def gcf_by_euclidean(self, nums: list) -> int:
         """Get the GCF of a list of nums with the Euclidean Algorithm."""
@@ -146,17 +127,11 @@ class GCFCalculator:
 
         # Get the common factors of all the numbers
         common_factors = set.intersection(*factors_sets)
-        return max(common_factors)
 
-    def factorization(self, n: int) -> list:
-        """Get all the factors of an integer"""
-        s = sqrt(n).__floor__()  # call .__floor__ to round down
-        factors = []
-        for i in range(2, s+1):
-            q = n/i  # quotient
-            if q.is_integer():
-                factors += [int(q), i]
-        return factors
+        # If relatively prime, return 1, else gcf
+        gcf = max(common_factors) if common_factors else 1
+
+        return gcf
 
     def gcf_by_prime_factorization(self, nums: list) -> int:
         prime_factors_lists = []
@@ -179,16 +154,66 @@ class GCFCalculator:
             # common to each number to occurrences list
             occurrences.extend([p for i in range(min_p)])
 
-        gcf = reduce(lambda x, y: x*y, occurrences)
+        # If relatively prime, return 1, else gcf
+        gcf = reduce(lambda x, y: x*y, occurrences) if occurrences else 1
+
         return gcf
 
+    def get_algo_data(self, algorithm, nums: list):
+        """Return gcf (int) and runtime (str) for a given GCF algorithm."""
 
-if __name__ == "__main__":
+        start = datetime.now()
+        gcf = algorithm(nums)
+        end = datetime.now()
+
+        td = (end - start).total_seconds() * 10**3
+        return gcf, f"{td:.03f}ms"
+
+    def get_gcf_and_algo_runtimes(self, nums):
+        """Return the GCF of the list of nums and the runtime of each
+        implemented algorithm as a string."""
+
+        gcf_values = {}
+        runtimes = ""
+
+        for name, algorithm in self.algos.items():
+            gcf, runtime = self.get_algo_data(algorithm, nums)
+            gcf_values[name] = gcf
+            runtimes += f"{name} Algorithm runtime: {runtime}\n"
+
+        self.bug_check(gcf_values)
+
+        num_str = self.convert_nums_to_str(nums)
+        gcf = list(gcf_values.values())[0]
+        gcf = "1 (relatively prime)" if gcf == 1 else gcf
+        results = f"{num_str}\nGCF: {gcf}\n{runtimes}"
+        return results
+
+
+def main():
+
+    selections = {
+        "1": ("Get GCF from user input of numbers", user_input),
+        "2": ("Quick print sample GCFs", sample_data)}
+
+    while True:
+        for k, v in selections.items():
+            print(f"{k}: {v[0]}")
+        usr_in = input("Select function (q to quit): ").strip()
+        if usr_in == "q":
+            quit()
+        elif usr_in in selections:
+            print("")
+            selections[usr_in][1]()
+        else:
+            print("Invalid selection.")
+
+
+def sample_data():
     list_one = [18, 27]
     list_two = [20, 50, 120]
     list_three = [182664, 154875, 137688]
-    list_four = [182664, 1548787456, 1345877688, 84, 849845486, 9848754542, 498498746546548]
-
+    # list_four = [182664, 1548787456, 1345877688, 849845486, 9848754542, 498498746546548]
     gcf_one = GCFCalculator(list_one).results
     print(gcf_one)
     gcf_two = GCFCalculator(list_two).results
@@ -197,3 +222,24 @@ if __name__ == "__main__":
     print(gcf_three)
     # gcf_four = GCFCalculator(list_four).results
     # print(gcf_four)
+
+
+def user_input():
+    nums = []
+    while True:
+        try:
+            usr_in = input(
+                "Enter number (0 to finish entries): ").strip()
+            if usr_in == "0":
+                break
+            nums.append(int(usr_in))
+        except ValueError:
+            print("Input must be an integer.")
+
+    gcf_data = GCFCalculator(nums).results
+    print(f"\n{gcf_data}")
+
+
+if __name__ == "__main__":
+
+    main()
