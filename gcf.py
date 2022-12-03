633bf4b85c5a443a7016ecd659f06dcc7556ee96
Alessandro Ceserani <aceserani@gmail.com>
2022-12-03 05:30:28 -0600

    implement prime factorization algo

--- gcf.py
+++ gcf.py
@@ -4,6 +4,50 @@
 
 from datetime import datetime
 from math import sqrt
+from functools import reduce
+
+
+class Node:
+    def __init__(self, value: int):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class PrimeFactorTree:
+    """Generate a binary tree of all the prime factor pairs of a number."""
+
+    def __init__(self, value: int):
+        # Initialize an empty list if this is the first call.
+        self.primes = []
+        self.root = self.prime_factorization(value)
+
+    def prime_factorization(self, n: int):
+        """Prime Factor Tree generator."""
+
+        node = Node(n)
+
+        if self.is_prime(n):
+            self.primes.append(n)
+            return node
+
+        s = sqrt(n).__floor__()  # call .__floor__ to round down
+
+        for i in range(2, s + 1):
+            if node.value % i == 0:
+                # print(f"n: {n} i: {i} n/i: {int(n/i)}")
+                node.left == self.prime_factorization(i)
+                node.right == self.prime_factorization(int(n/i))
+                break
+
+        return node
+
+    def is_prime(self, n):
+        """Return True if n is prime, else return False."""
+        for i in range(2, int(n/2)):
+            if (n % i) == 0:
+                return False
+        return True
 
 
 def get_gcf_and_runtime(selection: str, nums: list):
@@ -17,13 +61,13 @@ def get_gcf_and_runtime(selection: str, nums: list):
     print(f"Runtime of {selection} Algorithm: {td:.03f}ms")
 
 
-def euclidean(nums: list) -> int:
+def gcf_by_euclidean(nums: list) -> int:
 
     if len(nums) > 2:
         # Replace the first two nums in the list with their GCF
-        a = euclidean(nums[0:2])
+        a = gcf_by_euclidean(nums[0:2])
         nums = [a]+nums[2:]
-        return euclidean(nums)
+        return gcf_by_euclidean(nums)
 
     a = nums[0]
     b = nums[1]
@@ -42,36 +86,61 @@ def euclidean(nums: list) -> int:
     # Write A in quotient remainder form (A = B⋅Q + R)
     # print(f"{a} = {b} * {q} + {r}\n")
     # Find GCD(B,R) using the Euclidean Algorithm since GCD(A,B) = GCD(B,R)
-    return euclidean([b, r])  # def euclidean(nums):
+    return gcf_by_euclidean([b, r])  # def euclidean(nums):
 
 
-def factoring(nums: list) -> int:
-
-    def factorization(n: int) -> list:
-        """Get all the factors of an integer"""
-        s = sqrt(n).__floor__()
-        factors = []
-        for i in range(1, s+1):
-            q = n/i  # quotient
-            if q.is_integer():
-                factors += [int(q), i]
-        return set(factors)
+def gcf_by_factoring(nums: list) -> int:
 
     # Get sets of the factors of each number and add them to a list
     factors_sets = []
     for num in nums:
-        factors_sets.append(factorization(num))
+        factors_sets.append(set(factorization(num)))
 
     # Get the common factors of all the numbers
     common_factors = set.intersection(*factors_sets)
     return max(common_factors)
 
 
-algos = {
-    "Euclidean": euclidean,
-    "Factoring": factoring,
-}
+def factorization(n: int) -> list:
+    """Get all the factors of an integer"""
+    s = sqrt(n).__floor__()  # call .__floor__ to round down
+    factors = []
+    for i in range(2, s+1):
+        q = n/i  # quotient
+        if q.is_integer():
+            factors += [int(q), i]
+    return factors
 
+
+def gcf_by_prime_factorization(nums: list) -> int:
+    prime_factors_lists = []
+    for num in nums:
+        primes = PrimeFactorTree(num).primes
+        prime_factors_lists.append(primes)
+
+    common_factors = set([p for primes in prime_factors_lists for p in primes])
+
+    occurrences = []
+    for p in common_factors:
+        min_p = float('inf')
+        for primes in prime_factors_lists:
+            # Count the occurrences of p for this list of prime factors
+            p_count = primes.count(p)
+            if p_count < min_p:
+                min_p = p_count
+        # Add the highest number of occurrences of each prime factor that is
+        # common to each number to occurrences list
+        occurrences.extend([p for i in range(min_p)])
+
+    gcf = reduce(lambda x, y: x*y, occurrences)
+    return gcf
+
+
+algos = {
+    "Euclidean": gcf_by_euclidean,
+    "Factoring": gcf_by_factoring,
+    "Prime Factorization": gcf_by_prime_factorization,
+}
 
 if __name__ == "__main__":
     list_one = [18, 27]
@@ -81,9 +150,12 @@ if __name__ == "__main__":
     print("----")
     get_gcf_and_runtime("Euclidean", list_one)
     get_gcf_and_runtime("Factoring", list_one)
+    get_gcf_and_runtime("Prime Factorization", list_one)
     print("----")
     get_gcf_and_runtime("Euclidean", list_two)
     get_gcf_and_runtime("Factoring", list_two)
+    get_gcf_and_runtime("Prime Factorization", list_three)
     print("----")
     get_gcf_and_runtime("Euclidean", list_three)
     get_gcf_and_runtime("Factoring", list_three)
+    get_gcf_and_runtime("Prime Factorization", list_three)
